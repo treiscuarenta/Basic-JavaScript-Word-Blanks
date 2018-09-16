76cf7a6a2e79f188818a8810a7be08a594799efd
Fintan McGee <fintanmcgee1981@gmail.com>
2018-09-16 15:59:24 +0100

    Create word-blanks.js

---
+++ word-blanks.js
@@ -0,0 +1,13 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  //var result = "";
+
+var result = "The "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
+wordBlanks("cat", "little", "hit", "slowly");
